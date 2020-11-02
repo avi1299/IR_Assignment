58767930c621c2104361a80664ca0d93645b2af0
avi1299 <avi1299@gmail.com>
2020-11-03 00:56:48 +0530

    Few more changes

--- clean_text.py
+++ clean_text.py
@@ -1,11 +1,33 @@
 import re
+import string
+from nltk.tokenize import word_tokenize
 
 pattern = re.compile("<(/)?a[^>]*>")
-f=open('./Text_corpus/wiki_00', 'r' )
-o=open('./temp','w')
+
+
+f=open('./test.txt', 'r' , encoding='utf8')
+o=open('./temp','w', encoding='utf8')
 content = f.read()
 content=str(content)
 content_new = re.sub(pattern,"", content)
+
+#Replaces punctuations with spaces
+translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+
+#Removes weird punctuations
+content_new = content_new.translate(translator)
+content_new=content_new.translate(str.maketrans('', '', "‘’’"))
+
+#Makes all text lower case
+content_new=content_new.lower()
+
+#Creating a dictionary of words in the documents
+#Will be used later
+processed_text = word_tokenize(content_new)
+dictionary=list(set(processed_text))
+print(dictionary)
+
+
 o.write(content_new)
 f.close()
 o.close()
