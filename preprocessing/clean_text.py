f7aff79b19b15b5004ae47d3c7dd46878ba61621
Manan Paneri <39223931+manan-paneri-99@users.noreply.github.com>
2020-11-04 02:28:38 +0530

    Update clean_text.py

--- preprocessing/clean_text.py
+++ preprocessing/clean_text.py
@@ -1,13 +1,36 @@
 import re
+import string
+from nltk.tokenize import word_tokenize
 
-pattern = re.compile("<(/)?a[^>]*>")
-punctuation = re.compile(",")
-f=open('./Text_corpus/wiki_00', 'r', encoding='utf8')
-o=open('./temp','w', encoding='utf8')
-content = f.read()
-content=str(content)
-content_new = re.sub(pattern, " ", content)
-content_new = re.sub(punctuation, " ", content_new)
-o.write(content_new)
-f.close()
-o.close()
+
+def return_dictionary():
+    pattern = re.compile("<(/)?a[^>]*>")
+
+    f = open('./test.txt', 'r', encoding='utf8')
+    o = open('./temp', 'w', encoding='utf8')
+    content = f.read()
+    content = str(content)
+    content_new = re.sub(pattern, "", content)
+
+    # Replaces punctuations with spaces
+    translator = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+
+    # Removes weird punctuations
+    content_new = content_new.translate(translator)
+    content_new=content_new.translate(str.maketrans('', '', "‘’’"))
+
+    # Makes all text lower case
+    content_new = content_new.lower()
+
+    # Creating a dictionary of words in the documents
+    # Will be used later
+
+    processed_text = word_tokenize(content_new)
+    dictionary = set(processed_text)
+    # print(dictionary)
+    # print(len(dictionary))
+
+    o.write(content_new)
+    f.close()
+    o.close()
+    return dictionary
