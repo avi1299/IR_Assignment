868d880dbf013cf0f1edfa29c4a03f221dd23db8
Manan Paneri <39223931+manan-paneri-99@users.noreply.github.com>
2020-11-22 14:22:47 +0530

    Update test_queries.py

--- test_queries.py
+++ test_queries.py
@@ -2,6 +2,11 @@ from query_processer import search
 
 # Taking query input
 query = input("Type the query: ")
-search(query, open_web=False, use_zones=False, enable_query_relaxation=2)
-print("\n")
+search(query, open_web=False, use_zones=False, enable_query_relaxation=False)
+print('\n')
+search(query, open_web=False, use_zones=False, enable_query_relaxation=1)
+
+
+
+
 # search(query, open_web=False, use_zones=True)
